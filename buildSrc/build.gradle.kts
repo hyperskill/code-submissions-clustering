b0c49d0fb951bb82efffcfe2bf88f2b0b822cda8
eartser <eartser19@gmail.com>
2022-08-03 21:51:23 +0300

    add protobuf dependency

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,4 +11,5 @@ dependencies {
     implementation(libs.kotlin.gradle.plugin)
     implementation(libs.diktat.gradle.plugin)
     implementation(libs.detekt.gradle.plugin)
+    implementation(libs.protobuf.plugin)
 }
