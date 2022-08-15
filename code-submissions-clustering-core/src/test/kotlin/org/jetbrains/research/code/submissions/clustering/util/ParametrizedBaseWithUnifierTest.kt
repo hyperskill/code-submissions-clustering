d25f4a0c9faa02e725acb9f1c9281c544a900b29
Anastasiia.Birillo <nbirillo@mail.ru>
2022-08-15 13:16:11 +0200

    Upload tests report

--- code-submissions-clustering-core/src/test/kotlin/org/jetbrains/research/code/submissions/clustering/util/ParametrizedBaseWithUnifierTest.kt
+++ code-submissions-clustering-core/src/test/kotlin/org/jetbrains/research/code/submissions/clustering/util/ParametrizedBaseWithUnifierTest.kt
@@ -7,8 +7,8 @@ import org.jetbrains.research.code.submissions.clustering.ProtoSubmissionsEdge
 import org.jetbrains.research.code.submissions.clustering.ProtoSubmissionsGraph
 import org.jetbrains.research.code.submissions.clustering.ProtoSubmissionsNode
 import org.jetbrains.research.code.submissions.clustering.load.context.SubmissionsGraphContext
-import org.jetbrains.research.code.submissions.clustering.load.distance.measurers.gumtree.GumTreeDistanceMeasurer
 import org.jetbrains.research.code.submissions.clustering.load.context.builder.gumtree.GumTreeGraphContextBuilder
+import org.jetbrains.research.code.submissions.clustering.load.distance.measurers.gumtree.GumTreeDistanceMeasurer
 import org.jetbrains.research.code.submissions.clustering.load.unifiers.PyUnifier
 import org.jetbrains.research.pluginUtilities.util.ParametrizedBaseWithPythonSdkTest
 import org.junit.Ignore
@@ -40,7 +40,7 @@ open class ParametrizedBaseWithUnifierTest(testDataRoot: String) : ParametrizedB
 
     private fun List<ProtoSubmissionsNode>.sortedNodes() = this.sortedBy { it.id }
 
-    private fun List<ProtoSubmissionsEdge>.sortedEdges() = this.sortedWith(compareBy({it.from.id}, {it.to.id}))
+    private fun List<ProtoSubmissionsEdge>.sortedEdges() = this.sortedWith(compareBy({ it.from.id }, { it.to.id }))
 
     protected fun ProtoSubmissionsGraph.assertEquals(other: ProtoSubmissionsGraph) {
         assertEquals(this.verticesList.sortedNodes(), other.verticesList.sortedNodes())
