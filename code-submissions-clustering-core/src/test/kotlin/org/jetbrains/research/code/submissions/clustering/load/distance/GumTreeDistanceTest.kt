ffa16cb6b781126858c8011063b1dc5ded0957cb
Anastasiia.Birillo <nbirillo@mail.ru>
2022-10-19 14:42:50 +0200

    Fix distance calculation and add tests for it

---
+++ code-submissions-clustering-core/src/test/kotlin/org/jetbrains/research/code/submissions/clustering/load/distance/GumTreeDistanceTest.kt
@@ -0,0 +1,49 @@
+package org.jetbrains.research.code.submissions.clustering.load.distance
+
+import com.intellij.openapi.command.WriteCommandAction
+import org.jetbrains.research.code.submissions.clustering.load.context.builder.gumtree.converter.getTreeContext
+import org.jetbrains.research.code.submissions.clustering.load.distance.measurers.gumtree.GumTreeDistanceMeasurerByPsi
+import org.jetbrains.research.code.submissions.clustering.model.Language
+import org.jetbrains.research.code.submissions.clustering.util.ParametrizedBaseWithUnifierTest
+import org.jetbrains.research.code.submissions.clustering.util.asPsiFile
+import org.jetbrains.research.code.submissions.clustering.util.getTmpProjectDir
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class GumTreeDistanceTest : ParametrizedBaseWithUnifierTest(getTmpProjectDir()) {
+    private fun String.getTreeContext() =
+        this.asPsiFile(
+            Language.PYTHON,
+            mockPsiManager!!
+        ) { getTreeContext(it) }
+
+    @ParameterizedTest
+    @MethodSource("solutions")
+    fun testCalculateDistances(from: String, to: String, expectedDistance: Int) {
+        WriteCommandAction.runWriteCommandAction(mockProject) {
+            requireNotNull(mockProject)
+            requireNotNull(mockPsiManager)
+            val measurer = GumTreeDistanceMeasurerByPsi(mockProject!!)
+            val actions = measurer.calculateDistance(from.getTreeContext(), to.getTreeContext())
+            val dist = with(measurer) {
+                actions.calculateWeight()
+            }
+            assertEquals(expectedDistance, dist)
+        }
+    }
+    companion object {
+        private const val EMPTY_CODE = ""
+
+        @JvmStatic
+        fun solutions(): List<Arguments> = GumTreeDistanceCodeSnippetsData.codeSnippets.map {
+            listOf(
+                // from, to, distance
+                Arguments.of(it.code1, it.code2, it.distance),
+                Arguments.of(it.code2, it.code1, it.distance),
+                Arguments.of(it.code1, it.code1, 0),
+                Arguments.of(it.code2, it.code2, 0)
+            )
+        }.flatten() + Arguments.of(EMPTY_CODE, EMPTY_CODE, 0)
+    }
+}
---
+++ code-submissions-clustering-core/src/test/kotlin/org/jetbrains/research/code/submissions/clustering/load/distance/GumTreeDistanceTestCodeSnippets.kt
@@ -0,0 +1,76 @@
+@file:Suppress("KDOC_NO_CONSTRUCTOR_PROPERTY", "MAGIC_NUMBER")
+
+package org.jetbrains.research.code.submissions.clustering.load.distance
+
+data class GumTreeDistanceTestCodeSnippets(
+    val code1: String,
+    val code2: String,
+    val distance: Int
+)
+
+object GumTreeDistanceCodeSnippetsData {
+    private val smallChanges = GumTreeDistanceTestCodeSnippets(
+        """
+import sys
+
+
+def f1(f1_p1, f1_p2):
+    print(abs((f1_p1 % f1_p2) - pow(f1_p2, f1_p1)))
+
+
+f1(int(input()), int(input()))
+sys.exit()
+                    """.trimIndent(), """
+def f1(f1_p1, f1_p2):
+    print(abs(((f1_p1 % f1_p2) - pow(f1_p2, f1_p1))))
+                    """.trimIndent(),
+        5
+    )
+    private val middleChanges1 = GumTreeDistanceTestCodeSnippets(
+        """
+def f1(f1_p1, f1_p2):
+    print(abs(((f1_p1 % f1_p2) - pow(f1_p2, f1_p1))))
+                    """.trimIndent(),
+        """
+def f1(f1_p1, f1_p2):
+    print(abs(pow(f1_p2, f1_p1) - (f1_p1 // f1_p2) - f1_p1))
+                    """.trimIndent(),
+        12
+    )
+    private val middleChanges2 = GumTreeDistanceTestCodeSnippets(
+        """
+def f1(f1_p1, f1_p2): 
+    print(abs((f1_p1 % f1_p2) - (pow(f1_p2, f1_p1))))
+                    """.trimIndent(),
+        """
+import math
+
+
+def f1(f1_p1, f1_p2): 
+    print(int(math.fabs(f1_p1 % f1_p2 - math.pow(f1_p2, f1_p1))))
+                    """.trimIndent(),
+        13
+    )
+    private val bigChanges = GumTreeDistanceTestCodeSnippets(
+        """
+v1 = [float(v2) for v2 in input().split()]
+v3 = []
+for v4 in v1:
+    v3.append(v4 * 1000)
+v5 = []
+for v4 in sorted(v3, reverse=True):
+    v5.append(v4 / 1000)
+print(v5)
+""".trimIndent(),
+        """
+print([float(v1) for v1 in sorted(input().split(), reverse=True)])
+        """.trimIndent(),
+        31
+    )
+    val codeSnippets = listOf(
+        smallChanges,
+        middleChanges1,
+        middleChanges2,
+        bigChanges
+    )
+}
