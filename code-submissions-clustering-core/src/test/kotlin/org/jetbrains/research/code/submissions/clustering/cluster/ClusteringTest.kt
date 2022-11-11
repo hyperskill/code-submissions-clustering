4818c1d12cfcdaeb3f0ba0f2acdf2727cad3694f
eartser <eartser19@gmail.com>
2022-11-11 19:41:14 +0300

    small fix

--- code-submissions-clustering-core/src/test/kotlin/org/jetbrains/research/code/submissions/clustering/cluster/ClusteringTest.kt
+++ code-submissions-clustering-core/src/test/kotlin/org/jetbrains/research/code/submissions/clustering/cluster/ClusteringTest.kt
@@ -218,7 +218,7 @@ class ClusteringTest : ParametrizedBaseWithUnifierTest(getTmpProjectDir()) {
 
         @JvmStatic
         fun getTestDataFromBin(): List<Arguments> {
-            val binDirName = "bin.data/"
+            val binDirName = "bin.data"
             val classLoader = Thread.currentThread().contextClassLoader
             val binDir = classLoader.getResource(binDirName)?.toURI()?.let { Paths.get(it) }
 
