b820f9201c8b1e91c0b1035b8cbc66f698795ba1
eartser <eartser19@gmail.com>
2023-01-25 00:06:15 +0100

    add transformations stats logging and move anonymization out of cycle

--- code-submissions-clustering-core/src/main/kotlin/org/jetbrains/research/code/submissions/clustering/load/unifiers/AbstractUnifier.kt
+++ code-submissions-clustering-core/src/main/kotlin/org/jetbrains/research/code/submissions/clustering/load/unifiers/AbstractUnifier.kt
@@ -15,6 +15,7 @@ import org.jetbrains.research.code.submissions.clustering.util.getTmpProjectDir
 import org.jetbrains.research.code.submissions.clustering.util.reformatInWriteAction
 import org.jetbrains.research.ml.ast.transformations.Transformation
 import java.util.logging.Logger
+import kotlin.system.measureTimeMillis
 
 /**
  * Abstract unifier producing unifying transformations over code submissions.
@@ -28,11 +29,24 @@ abstract class AbstractUnifier(
     abstract val language: Language
     abstract val transformations: List<Transformation>
     private val codeToUnifiedCode = HashMap<String, String>()
+    private val transformationsNumber = mutableMapOf<String, Int>()
+    private val transformationsExecTime = mutableMapOf<String, Long>()
+
+    private fun Transformation.forwardApplyMeasured(psiTree: PsiFile) {
+        val previousTree = psiTree.copy()
+        val executionTime = measureTimeMillis {
+            forwardApply(psiTree)
+        }
+        val isApplied = !(previousTree?.textMatches(psiTree) ?: false)
+        if (isApplied) {
+            transformationsNumber[key] = transformationsNumber.getOrDefault(key, 0) + 1
+        }
+        transformationsExecTime[key] = transformationsExecTime.getOrDefault(key, 0) + executionTime
+    }
 
     @Suppress("TooGenericExceptionCaught")
     private fun PsiFile.applyTransformations(transformations: List<Transformation>, previousTree: PsiElement? = null) {
         logger.fine { "Tree Started: ${this.text}" }
-        anonymization?.forwardApply(this)
 
         val psiDocumentManager = this.project.service<PsiDocumentManager>()
         val document = this.let { psiDocumentManager.getDocument(it) }
@@ -42,7 +56,7 @@ abstract class AbstractUnifier(
                     psiDocumentManager.commitDocument(document)
                 }
                 logger.finer { "Transformation Started: ${it.key}" }
-                it.forwardApply(this)
+                it.forwardApplyMeasured(this)
                 logger.finer { "Transformation Ended: ${it.key}" }
             }
         } catch (e: Throwable) {
@@ -57,9 +71,11 @@ abstract class AbstractUnifier(
 
     @Suppress("TOO_MANY_LINES_IN_LAMBDA")
     fun Submission.unify(): Submission {
+        clearStats()
         val code = codeToUnifiedCode.getOrDefault(this.code, this.code.asPsiFile(language, psiManager) {
             ApplicationManager.getApplication().invokeAndWait {
                 ApplicationManager.getApplication().runWriteAction {
+                    anonymization?.forwardApplyMeasured(it)
                     var iterationNumber = 0
                     do {
                         ++iterationNumber
@@ -69,16 +85,46 @@ abstract class AbstractUnifier(
                         logger.finer { "Current text[$iterationNumber]:\n${it.text}\n\n" }
                     } while (!previousTree.textMatches(it.text) && iterationNumber <= MAX_ITERATIONS && codeToUnifiedCode[it.text] == null)
                     logger.fine { "Tree Ended[[$iterationNumber]]: ${it.text}\n\n\n" }
+                    logger.info { "Total iterations number: $iterationNumber" }
                 }
             }
             codeToUnifiedCode.getOrPut(it.text) { it.reformatInWriteAction().text }
         })
+        logger.info { logTransformationStats() }
         return this.copy(code = code)
     }
 
     fun clearCache() = codeToUnifiedCode.clear()
 
+    private fun clearStats() {
+        transformationsExecTime.clear()
+        transformationsNumber.clear()
+    }
+
+    private fun logTransformationStats(): String = buildString {
+        appendLine("Transformations statistics:")
+        val logTransformations = anonymization?.let {
+            listOf(it) + transformations
+        } ?: transformations
+        logTransformations.forEach {
+            appendLine(
+                logStats(
+                    it.key,
+                    transformationsExecTime.getOrDefault(it.key, 0),
+                    transformationsNumber.getOrDefault(it.key, 0)
+                )
+            )
+        }
+    }
+
+    private fun logStats(key: String, execTime: Long, number: Int): String = buildString {
+        append(key.padEnd(LOG_PADDING))
+        append("$execTime ms".padEnd(LOG_PADDING))
+        append("$number times applied")
+    }
+
     companion object {
+        const val LOG_PADDING = 30
         const val MAX_ITERATIONS = 50
     }
 }
