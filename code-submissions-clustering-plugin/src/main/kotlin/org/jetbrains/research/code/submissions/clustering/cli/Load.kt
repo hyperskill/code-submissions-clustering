2d560e7ee770c52b1025c8da844bd06f9e7e795e
eartser <eartser19@gmail.com>
2023-08-08 20:32:00 +0200

    small fixes

--- code-submissions-clustering-plugin/src/main/kotlin/org/jetbrains/research/code/submissions/clustering/cli/Load.kt
+++ code-submissions-clustering-plugin/src/main/kotlin/org/jetbrains/research/code/submissions/clustering/cli/Load.kt
@@ -1,19 +1,21 @@
 package org.jetbrains.research.code.submissions.clustering.cli
 
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.types.file
 import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.dataframe.io.readCSV
 import org.jetbrains.research.code.submissions.clustering.util.loadGraph
+import java.io.File
 
 @Suppress("WRONG_ORDER_IN_CLASS_LIKE_STRUCTURES")
 class Load : AbstractGraphBuilder(
     name = "load",
     help = "Load submissions graph from .csv file"
 ) {
-    private val inputFile: String by argument(
+    private val inputFile: File by argument(
         "inputFile",
         help = "Input .csv file with code submissions",
-    )
+    ).file(mustExist = true)
 
     override fun run() {
         startRunner {
