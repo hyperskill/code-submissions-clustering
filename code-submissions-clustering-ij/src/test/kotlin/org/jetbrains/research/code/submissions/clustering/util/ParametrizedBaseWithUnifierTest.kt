ea519ac0a0cdb286069deaf447ac0594345160c8
eartser <eartser19@gmail.com>
2023-09-11 17:48:40 +0200

    try to fix comparing in tests

--- code-submissions-clustering-ij/src/test/kotlin/org/jetbrains/research/code/submissions/clustering/util/ParametrizedBaseWithUnifierTest.kt
+++ code-submissions-clustering-ij/src/test/kotlin/org/jetbrains/research/code/submissions/clustering/util/ParametrizedBaseWithUnifierTest.kt
@@ -5,6 +5,7 @@ import com.intellij.psi.PsiManager
 import com.intellij.testFramework.fixtures.CodeInsightTestFixture
 import kotlinx.coroutines.runBlocking
 import org.jetbrains.kotlinx.dataframe.DataFrame
+import org.jetbrains.research.code.submissions.clustering.ProtoSubmissionInfo
 import org.jetbrains.research.code.submissions.clustering.ProtoSubmissionsEdge
 import org.jetbrains.research.code.submissions.clustering.ProtoSubmissionsGraph
 import org.jetbrains.research.code.submissions.clustering.ProtoSubmissionsNode
@@ -37,13 +38,34 @@ open class ParametrizedBaseWithUnifierTest(testDataRoot: String) : ParametrizedB
         }
     }
 
-    private fun List<ProtoSubmissionsNode>.sortedNodes() = this.sortedBy { it.id }
+    private fun List<ProtoSubmissionsNode>.sortedNodes() = this.sortedBy { it.code }
 
-    private fun List<ProtoSubmissionsEdge>.sortedEdges() = this.sortedWith(compareBy({ it.from.id }, { it.to.id }))
+    private fun List<ProtoSubmissionInfo>.sortedInfo() = this.sortedBy { it.id }
+
+    private fun List<ProtoSubmissionsEdge>.sortedEdges() = this.sortedWith(compareBy({ it.from.code }, { it.to.code }))
+
+    private fun ProtoSubmissionsNode.equalsNode(other: ProtoSubmissionsNode) =
+        (this.stepId == other.stepId) and (this.code == other.code) and (this.infoList.sortedInfo() == other.infoList.sortedInfo())
+
+    private fun List<ProtoSubmissionsNode>.assertEqualsNodes(other: List<ProtoSubmissionsNode>) {
+        assert(this.size == other.size)
+        this.sortedNodes().zip(other.sortedNodes()).forEach { (first, second) -> assert(first.equalsNode(second)) }
+    }
+
+    private fun List<ProtoSubmissionsEdge>.assertEqualsEdges(other: List<ProtoSubmissionsEdge>) {
+        assert(this.size == other.size)
+        this.sortedEdges().zip(other.sortedEdges()).forEach { (first, second) ->
+            assert(first.weight == second.weight)
+            assert(
+                ((first.from.equalsNode(second.from)) and (first.to.equalsNode(second.to)))
+                        or ((first.from.equalsNode(second.to)) and (first.to.equalsNode(second.from)))
+            )
+        }
+    }
 
     protected fun ProtoSubmissionsGraph.assertEquals(other: ProtoSubmissionsGraph) {
-        assertEquals(this.verticesList.sortedNodes(), other.verticesList.sortedNodes())
-        assertEquals(this.edgesList.sortedEdges(), other.edgesList.sortedEdges())
+        this.verticesList.assertEqualsNodes(other.verticesList)
+        this.edgesList.assertEqualsEdges(other.edgesList)
     }
 
     protected fun Clustering<SubmissionsNode>.assertEquals(other: Clustering<SubmissionsNode>) {
